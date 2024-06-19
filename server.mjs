d3ca07be8ac39f3b2e5a05a2a294712723fc9c0d
Rafida <zakariarafida81@gmail.com>
2024-06-19 09:27:50 +0000

    learning nodejs

---
+++ server.mjs
@@ -0,0 +1,16 @@
+import{createServer} from 'node:http';
+
+//req=request res=respond
+const server =createServer((req,res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Server is running');
+});
+
+server.listen(3000 , '127.0.0.1', () => {
+    console.log('Server is listening');
+});
+
+//Arrow Functions
+function handleRequest(){
+    const handleRequest= () =>{}
+}
